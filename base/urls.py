aab9f313cea7a257f4facee77aff76649ab15282
Sounak Majumder <sounakmajumder472@gmail.com>
2021-09-24 01:46:12 +0530

    login-logout

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, CustomLoginView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('', TaskList.as_view(), name='tasks'),
     path('tasks/<int:pk>/', TaskDetail.as_view(), name='task'),
     path('task-update/<int:pk>/', TaskUpdate.as_view(), name='task-update'),
--- base/views.py
+++ base/views.py
@@ -4,8 +4,17 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView
 
 # Create your views here.
+
+class CustomLoginView(LoginView):
+    template_name = 'base/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('tasks')
 
 class TaskList(ListView):
     model = Task
