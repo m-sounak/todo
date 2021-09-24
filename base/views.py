b4d8003471860178e704f1ce55295536bb38f77e
Sounak Majumder <sounakmajumder472@gmail.com>
2021-09-25 00:30:05 +0530

    Backend basic done

--- base/views.py
+++ base/views.py
@@ -44,6 +44,13 @@ class TaskList(LoginRequiredMixin, ListView):
         context = super().get_context_data(**kwargs)
         context['tasks'] = context['tasks'].filter(user=self.request.user)
         context['count'] = context['tasks'].filter(complete=False).count()
+
+        search_input = self.request.GET.get('search-area') or ''
+        if search_input:
+            context['tasks'] = context['tasks'].filter(title__icontains = search_input)
+
+        context['search_input'] = search_input
+
         return context
 
 class TaskDetail(LoginRequiredMixin, DetailView):
