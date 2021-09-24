09f248c8189f133a6229e1a97633a9bfe630b861
Sounak Majumder <sounakmajumder472@gmail.com>
2021-09-25 00:51:51 +0530

    minors

--- todo/settings.py
+++ todo/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--%%zjah(=$u0sp%tbnz!u%)(gg*&l6*qd4#)6t$rbwp9nh5lyb'
+SECRET_KEY = 'todo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
