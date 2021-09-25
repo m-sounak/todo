b8f6c200d7df9ace34c657ef6a83b1428a115214
Sounak Majumder <sounakmajumder472@gmail.com>
2021-09-25 21:49:23 +0530

    Heroku adjustments

--- todo/settings.py
+++ todo/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'todo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://todo-chores.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
