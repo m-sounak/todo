fc577c1bdf19002682532d278da749b9c15536d2
Sounak Majumder <sounakmajumder472@gmail.com>
2021-09-26 01:56:26 +0530

    Heroku adjustments 6

--- todo/settings.py
+++ todo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'todo'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://todo-chores.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['https://todo-chores.herokuapp.com/', '127.0.0.1']
 
 
 # Application definition
